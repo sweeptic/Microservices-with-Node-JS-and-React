4475afdc3b97210faed9c1b9cbdb9e62c4e7bd99
Attila <59293104+sweeptic@users.noreply.github.com>
2020-11-02 12:22:13 +0100

    41. updating comment content

--- comments/index.js
+++ comments/index.js
@@ -42,8 +42,30 @@ app.post('/posts/:id/comments', async (req, res, next) => {
   res.status(201).send(comments);
 });
 
-app.post('/events', (req, res, next) => {
+app.post('/events', async (req, res, next) => {
   console.log('Received Event', req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data;
+
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find(comment => comment.id === id);
+
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    });
+  }
 
   res.send({});
 });
