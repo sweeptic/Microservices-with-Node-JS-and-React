99853321a2752ff83d12f15db0991c4fbaaae2f2
Attila <59293104+sweeptic@users.noreply.github.com>
2020-11-02 07:48:05 +0100

    33. using the query service

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -7,7 +7,8 @@ const PostList = () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = useCallback(async () => {
-    const res = await axios.get('http://localhost:4000/posts');
+    const res = await axios.get('http://localhost:4002/posts'); //query service here
+    console.log(res.data);
     setPosts(res.data);
   }, []);
 
@@ -15,7 +16,7 @@ const PostList = () => {
     fetchPosts();
   }, [fetchPosts]);
 
-  console.log(posts);
+  // console.log(posts);
 
   const renderedPosts = Object.values(posts).map(post => {
     return (
@@ -26,7 +27,7 @@ const PostList = () => {
       >
         <div className='card-body'>
           <h3>{post.title}</h3>
-          <CommentList postId={post.id} />
+          <CommentList comments={post.comments} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
