3995a584e99fc005f40c792e2252b86e5d415d9e
Attila <59293104+sweeptic@users.noreply.github.com>
2020-11-06 16:28:49 +0100

    87. unique route paths

--- posts/index.js
+++ posts/index.js
@@ -13,7 +13,7 @@ app.get('/posts', (req, res, next) => {
   res.send(posts);
 });
 
-app.post('/posts', async (req, res, next) => {
+app.post('/posts/create', async (req, res, next) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
 
